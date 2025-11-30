5a5bc6b4b432e96d157f33437dc1f80fab67a097
mayank2130 <mayankthakur1712@gmail.com>
2025-11-30 17:27:25 +0530

    advert header

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,32 +4,36 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: "Whop App",
-	description: "My Whop App",
+  title: "Whop App",
+  description: "My Whop App",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				<WhopApp>{children}</WhopApp>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="whop-app-id" content="app_JxC65kDufm1sK9" />
+        <script src="https://whop-ads.vercel.app/api/w/js" defer />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <WhopApp>{children}</WhopApp>
+      </body>
+    </html>
+  );
 }
